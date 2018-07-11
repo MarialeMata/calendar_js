d344443b97583cbe508115233fe4659ef6da1000
Maria Mata <nina8708@gmail.com>
2018-07-10 23:08:34 -0400

    Add moment.js library and plugin for holidays. Create moment.js objects from the selected date range.

--- calendar.js
+++ calendar.js
@@ -19,9 +19,56 @@ function initializeForm() {
       } else if (nofDays < 1) {
         alert("Date can't be empty.")
       } else{
-        console.log("Clicked!");
+        createCalendarItems(startDate, nofDays, countryCode);
       }
     }
 
   });
+}
+
+// Create an array of CalendarItem objects for each month in the selected date
+function createCalendarItems(startDate, nofDays, countryCode) {
+
+  // Create moment.js objects from the selected range
+  startDate = moment(startDate);
+  let endDate = moment(startDate.clone().add(nofDays - 1, "days"));
+  let startYear = Number(startDate.format("YYYY"));
+  let endYear = Number(endDate.format("YYYY"));
+  let startMonth = Number(startDate.format("M"));
+  let endMonth = Number(endDate.format("M"));
+  let years = endYear - startYear;
+  countryName = getCountryName(countryCode.toUpperCase());
+
+  // Set the country name from country code for the holidays plugin
+  moment.modifyHolidays.set(countryName);
+
+  console.log(startDate);
+  console.log(endDate);
+  console.log(startYear);
+  console.log(endYear);
+  console.log(startMonth);
+  console.log(endMonth);
+  console.log(years);
+  console.log(countryName);
+}
+
+// List of countries supported by the moment.js holidays plugin
+let countries = {
+  "AR" : "Argentina",
+  "CA" : "Canada",
+  "HR" : "Croatia",
+  "DK" : "Denmark",
+  "FI" : "Finland",
+  "DE" : "Germany",
+  "IN" : "India",
+  "US" : "United States"
+};
+
+// Returns the country name from a given code. If not in list, default is US.
+function getCountryName(countryCode) {
+    if (countries.hasOwnProperty(countryCode)) {
+    return countries[countryCode];
+  } else {
+    return countries["US"];
+  }
 }
